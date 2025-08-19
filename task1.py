31fc0295f11baa462a085b11574c390b2156430a
jadu56 <107939386+jadu56@users.noreply.github.com>
2025-08-19 12:30:45 +0530

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,1 +1,12 @@
-
+a=input("Enter a number: ")
+b=input("Enter another number: ")
+a=float(a)
+b=float(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Addition =",c)
+print("Subtraction =",d)
+print("Multiplication =",e)
+print("Division =",f)
